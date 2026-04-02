8fb97646ae8051e6f4034f73bf05050f3470fc61
Oceantt <seba.parada.rhime@gmail.com>
2026-04-01 21:43:22 -0300

    Solo falta la funcion eliminar actividad

--- src/Tallerss/Tallers1.java
+++ src/Tallerss/Tallers1.java
@@ -3,161 +3,55 @@ package Tallerss;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Tallers1 {
-	public static void main(String[] args) throws FileNotFoundException {
-
+	public static void main(String[] args) throws IOException {
 		Scanner scanner = new Scanner(System.in);
-
-
-
-	String RutaRegistros = "C:\\Users\\Usuario\\Downloads\\Registros.txt"; 
-
-	//cambiar estos strings segun posicion del archivo
-
-	String RutaUsuarios = "C:\\Users\\Usuario\\Downloads\\Usuarios.txt";
-
-
+		
+		//cambiar estos strings segun posicion del archivo	
+String RutaRegistros = "C:\\Users\\User\\Downloads\\Registros.txt"; 
+String RutaUsuarios = "C:\\Users\\User\\Downloads\\Usuarios.txt";
+File registros = new File(RutaRegistros);
+File usuarios = new File(RutaUsuarios);
 
 		String respuesta = "";
-
 		String respuestados = "";
-
-
-
-
-
-
-
 		do {
-
-
-
-
-
-
-
 		System.out.println("1) Menu de Usuarios\r\n"
-
-
-
 		+ "2) Menu de Analisis\r\n"
-
-
-
 		+ "3) Salir");
-
-
-
-
-
-
-
+		
 		respuesta = scanner.nextLine();
-
-
-
-
-
-
-
+		
 		if ((!respuesta.equals("1") && !respuesta.equals("2") && !respuesta.equals("3"))) {
-
-
-
-
-
-
-
-		System.out.println("Respuesta incorrecta");
-
-
-
-
-
-
-
-		}
-
-
-
-
-
-
-
+			
+		System.out.println("Respuesta incorrecta");}
+		
 		} while (!respuesta.equals("1") && !respuesta.equals("2") && !respuesta.equals("3"));
-
-
-
-
-
-
-
+		
 		while (!respuesta.equals("3") && !respuestados.equals("5")) {
-
 		if (respuesta.equals("3")) {
-
 		break;
 
 		}
-
-
-
-
-
-
-
 		int respuesta1 = Integer.parseInt(respuesta);
-
-
-
-
-
-
 
 		if (respuesta1 == 1) {
 
-
-
-
-
-
-
 		boolean correcto = false;
-
-
 
 		String nombre = "";
 
 		String contraseña = "";
 
 
-
-		
-
-
-
-
-
-
-
 		while (correcto != true) {
-
-
-
 		System.out.print("Usuario: ");
 
-
-
 		nombre = scanner.nextLine();
-
-
-
-
-
-
 
 		System.out.print("Contraseña: ");
 
@@ -165,19 +59,7 @@ public class Tallers1 {
 
 		contraseña = scanner.nextLine();
 
-
-
-
-
-
-
 		try {
-
-
-
-
-
-
 
 		File archivo = new File(RutaUsuarios);
 
@@ -185,87 +67,31 @@ public class Tallers1 {
 
 		Scanner scarchivo = new Scanner(archivo);
 
-
-
-
-
-
-
 		while (scarchivo.hasNextLine()) {
-
-
-
-
-
-
 
 		String linea = scarchivo.nextLine();
 
-
-
-
-
-
-
 		String[] partes = linea.split(";");
-
-
-
 		String nombreuser = partes[0];
-
-
 
 		String contrauser = partes[1];
 
-
-
-
-
-
-
 		if (nombre.equals(nombreuser) && contraseña.equals(contrauser)) {
 
-
-
-
-
-
-
 		System.out.println("\nAcceso Correcto!");
-
-
-
 		correcto = true;
-
-
-
 		break;
 
-
-
 		}
-
-
 
 		}
 
 		scarchivo.close();
-
-
-
 		if (correcto != true) {
-
-
-
 		System.out.println("Usuario o contraseña incorrectos intente nuevamente");
-
-
-
 		}
 
 		} catch (Exception error) {
-
-
 
 		System.out.println("Error al leer el archivo");
 
@@ -273,12 +99,11 @@ public class Tallers1 {
 
 		}
 
-
-
 		}
-		System.out.println("Bienvenido " + nombre + "!");
-		while(!respuestados.equals("5")) {
 
+		System.out.println("Bienvenido " + nombre + "!");
+
+		while(!respuestados.equals("5")) {
 		do {
 
 		System.out.println("Que deseas realizar?\r\n"
@@ -289,15 +114,22 @@ public class Tallers1 {
 		+ "4) Cambiar contraseña.\r\n"
 		+ "5) Salir.");
 		respuestados = scanner.nextLine();
-
 		if ((!respuestados.matches("[1-5]"))) {
+
 		System.out.println("Respuesta incorrecta");
 
 		}
 
+
 		} while (!respuestados.matches("[1-5]"));
 
-		if (respuestados.equals("5")) {break;}
+		
+
+		if (respuestados.equals("5")) {
+
+		break;
+
+		}
 
 		else if (respuestados.equals("1")) {
 
@@ -311,7 +143,11 @@ public class Tallers1 {
 
 		boolean pase = false;
 
+
+
 		do {
+
+		
 
 		System.out.print("indique la fecha de la actividad DD/MM/AAAA: ");
 
@@ -365,18 +201,16 @@ public class Tallers1 {
 
 		try {
 
-		Integer.parseInt(tiempo);
-
+	 int tiempo2 = Integer.parseInt(tiempo);
+if (tiempo2 >= 0) {
 		pase = true;
-
+		
 		}
 
+		}
 		catch(NumberFormatException a) {
 
 		System.out.println("Tiempo invalido");
-
-
-
 		}
 
 		}while(!pase);
@@ -394,9 +228,6 @@ public class Tallers1 {
 		escribir.append("\n"+añadiract);
 
 		escribir.close();
-
-		
-
 		}
 
 		catch(Exception f) {
@@ -404,358 +235,221 @@ public class Tallers1 {
 		System.out.println("Archivo no encontrado");
 
 		}
-
-		
-
-		
-
 		}
 
 		else if (respuestados.equals("2")) {
+String[] acts = new String[300];
+String[] fechas = new String[300];
+String[] nombres = new String[300];
+String[] lineacompleta= new String[300];
+String[] actsuser = new String[300];
+Scanner r = new Scanner(registros);
+int contadorreal = 0;
+while (r.hasNextLine()) {
+String linea = r.nextLine();
 
-		System.out.println("Que actividad deseas modificar?: ");
+lineacompleta[contadorreal] = linea;
+contadorreal +=1;
 
-		System.out.println("0) Regresar");
+}
 
 
 
+int respuestamodificar;
+int totalactividades = contadorreal;
+while(true) {
+System.out.println("0) Regresar");
+int contadoruser = 0;
+for (int i = 0; i < contadorreal; i++) {
+	String[] partes = lineacompleta[i].split(";");
+	if (partes[0].equals(nombre)) {
+		actsuser[contadoruser] = lineacompleta[i];
+		contadoruser+=1;
+		System.out.println(contadoruser+") "+lineacompleta[i]);
 		
-
-		try {
-
-		String actividadparamodificar = "";
-
-		int numactividadmodificar = 0;
-
-		File registros = new File(RutaRegistros);
-
-		Scanner lectura = new Scanner(registros);
-
-		int contador = 1;
-
-		while(lectura.hasNextLine()) {
-
-		String linea = lectura.nextLine();
-
-		String[] partes2 = linea.split(";");
-
-		String nombreregistro = partes2[0];
-
-		
-
-		if (nombreregistro.equals(nombre)) {
-
-		System.out.println(contador+") "+linea);
-
-		contador += 1;
-
-
-
-		}
-
-		
-
-		}
-
-		boolean pasar = false;
-
-		
-
-		while (pasar == false) {
-
-		
-
-		try {
-
-		System.out.println("Que actividad deseas modificar?: ");
-
-		int actmodificar = scanner.nextInt();
-
-		if (actmodificar == 0) {
-
+				
+	}
+	
+}
+while (true) {
+try {
+	System.out.print("\nQue actividad deseas modificar?: ");
+	 respuestamodificar = scanner.nextInt();
+	if (respuestamodificar <= contadorreal && respuestamodificar >= 0) {
 		break;
+	}
+	System.out.println("Numero de actividad invalido");
+}
+catch(Exception o) {
+	System.out.println("Actividad inexistente");
+	scanner.nextLine();
+}
+}
+if (respuestamodificar == 0) {break;}
+int actmodificar = respuestamodificar-1;
+System.out.println(actsuser[actmodificar]);
+while(true) {
+while(true) {
+try {
+	System.out.println("Que deseas modificar?\r\n"
+			+ "\r\n"
+			+ "0) Regresar.\r\n"
+			+ "1) Fecha\r\n"
+			+ "2) Duracion\r\n"
+			+ "3) Tipo de actividad");
 
-		}
-
-
-
-		if (actmodificar > 0 && actmodificar <= contador-1) {
-
-		pasar = true;
-
-		}
-
-		else {System.out.println("Numero invalido");}
-
-
-
-		Scanner lectura2 = new Scanner(registros);
-
-		String linea = "";
-
-		for (int i = 1; i <= actmodificar; i++) {
-
-		linea = lectura2.nextLine();
-
-		String[] partes = linea.split(";");
-
-		if(!partes[0].equals(nombre)) {
-
-		i--;
-
-		}
-
-
-
-		}
-
-		System.out.println(linea);
-
-		actividadparamodificar = linea;
-
-		numactividadmodificar = actmodificar;
-
-		
-
-		
-
-		
-
-		
-
-		}
-
-		catch(InputMismatchException g){
-
-		System.out.println("Actividad irreconocible");
-
-		scanner.nextLine();
-
-		}
-
-		
-
-		System.out.println();
-
-		
-
-		
-
-		
-
-		lectura.close();
-
-		}
-
-		System.out.println();
-
-		
-
-		pasar = false;
-
-		int aaaa = 0;
-
-		while (pasar == false) {
-
-		try {
-
-		System.out.println("Que deseas modificar?\r\n"
-
-		+ "\r\n"
-
-		+ "0) Regresar.\r\n"
-
-		+ "1) Fecha\r\n"
-
-		+ "2) Duracion\r\n"
-
-		+ "3) Tipo de actividad");
-
-		aaaa = scanner.nextInt();
-
-		if (aaaa == 0) {
-
+	 respuestamodificar = scanner.nextInt();
+	if (respuestamodificar <= 3 && respuestamodificar >= 0) {
 		break;
+	}
+	System.out.println("Numero invalido");
+}
+catch(Exception o) {
+	System.out.println("Numero invalido");
+	scanner.nextLine();
+}
 
-		}
 
 
+}
 
-		if (aaaa > 0 && aaaa <= 3) {
+if(respuestamodificar==0) {break;}
+scanner.nextLine();
+ String fecha_nueva = "";
+if (respuestamodificar==1) {
+	boolean pase = false;
+	do {
+	System.out.print("indique la nueva fecha de la actividad DD/MM/AAAA: ");
 
-		pasar = true;
+	  fecha_nueva = scanner.nextLine();
+	if (!fecha_nueva.matches("\\d{2}/\\d{2}/\\d{4}")) {
+	System.out.println("Formato incorrecto. Usa DD/MM/AAAA");
+	continue;}
+	try {
+	String[] partes_fecha = fecha_nueva.split("/");
+	int diadia = Integer.parseInt(partes_fecha[0]);
+	int mesmes = Integer.parseInt(partes_fecha[1]);
+	if (diadia <=31 && mesmes <= 12 && diadia > 0 && mesmes > 0) {
+	pase = true;
+	}
+	}
+	catch(NumberFormatException e) {
+	System.out.println("Fecha invalida");
+	} 
+	}
+	while(!pase);
+String[] partesact = actsuser[actmodificar].split(";");
+partesact[1] = fecha_nueva;
+actsuser[actmodificar] = partesact[0]+";"+partesact[1]+";"+partesact[2]+";"+partesact[3];
+System.out.println("Actividad modificada con exito!");
+break;
+}
+else if (respuestamodificar==2) {
+	boolean pase = false;
+	String tiempo = "";
+	do {
 
-		}
+		System.out.print("Indique el tiempo que duro la actividad en horas: ");
 
-		else {System.out.println("Numero invalido");}
+		  tiempo = scanner.nextLine();
 
-		
-
-		
-
-		
-
-		}
-
-		catch(InputMismatchException g){
-
-		System.out.println("Actividad irreconocible");
-
-		scanner.nextLine();
-
-		}
-
-		
-
-		}
-
-		
-
-		
-
-		String[] partesmodificar = actividadparamodificar.split(";");
-
-		if (aaaa ==1) {
-
-		boolean pase = false;
-
-		scanner.nextLine();
-
-		String fecha_nueva = "";
-
-		do {
-
-		
-
-		System.out.print("indique la nueva fecha de la actividad DD/MM/AAAA: ");
-
-		fecha_nueva = scanner.nextLine();
-
-		if (!fecha_nueva.matches("\\d{2}/\\d{2}/\\d{4}")) {
-
-		System.out.println("Formato incorrecto. Usa DD/MM/AAAA");
-
-		continue;}
+		pase = false;
 
 		try {
 
-		String[] partes_fecha = fecha_nueva.split("/");
-
-		int diadia = Integer.parseInt(partes_fecha[0]);
-
-		int mesmes = Integer.parseInt(partes_fecha[1]);
-
-		if (diadia <=31 && mesmes <= 12 && diadia > 0 && mesmes > 0) {
-
+	 int tiempo2 = Integer.parseInt(tiempo);
+if (tiempo2 >= 0) {
 		pase = true;
-
+		
 		}
 
 		}
+		catch(NumberFormatException a) {
 
-		catch(NumberFormatException e) {
-
-		System.out.println("Fecha invalida");
-
-		
-
-		} 
-
+		System.out.println("Tiempo invalido");
 		}
 
-		while(!pase);
+		}while(!pase);
+	
+	String[] partesact = actsuser[actmodificar].split(";");
+	partesact[2] = tiempo;
+	actsuser[actmodificar] = partesact[0]+";"+partesact[1]+";"+partesact[2]+";"+partesact[3];
+	System.out.println("Actividad modificada con exito!");
 
-		partesmodificar[1] = fecha_nueva;
+	break;
+	
+}
+else if (respuestamodificar==3) {
+	System.out.print("Indique el nuevo tipo de actividad: ");
 
-		String añadiract = partesmodificar[0]+";"+partesmodificar[1]+";"+partesmodificar[2]+";"+partesmodificar[3];
+	  String TipoActNueva = scanner.nextLine();
+	  String[] partesact = actsuser[actmodificar].split(";");
+		partesact[3] = TipoActNueva;
+		actsuser[actmodificar] = partesact[0]+";"+partesact[1]+";"+partesact[2]+";"+partesact[3];
+		System.out.println("Actividad modificada con exito!");
 
-		contador -=1;
+		break;
+	
+}
 
-		try {
 
-		
 
-		Scanner lector3 = new Scanner(registros);
-
-		
-
-		
-
-		String[] lineastemporales = new String[300];
-
-		int total = 0;
-
-		while(lector3.hasNextLine()) {
-
-		String lineaaa = lector3.nextLine();
-
-		lineastemporales[total] = lineaaa;
-
-		total +=1;}
-
-		FileWriter escribir = new FileWriter(RutaRegistros);
-
-		for (int j = 0; j < total ; j++) {
-
-		if(numactividadmodificar==j) {
-
-		escribir.write(añadiract+"\n");
-
+break;
 		}
 
-		if (lineastemporales[j] != null) {
+int contadoro = 0;
+for (int i = 0; i < totalactividades; i++) {
+String[] partes = lineacompleta[i].split(";");
+if (partes[0].equals(nombre)) {
+	lineacompleta[i] =actsuser[contadoro];
+	contadoro +=1;
+	
+	
+}
+}
+FileWriter escribir = new FileWriter(registros);
 
-		escribir.write(lineastemporales[j]+"\n");
-
-		}
-
-
-
-		
-
-		
-
-
-
-		}
-
-		
-
-		escribir.close();
-
-		
-
-		}
-
-		catch(Exception f) {
-
-		System.out.println("Archivo no encontrado");
-
+for (int i = 0; i < totalactividades; i++) {
+escribir.write(lineacompleta[i]+"\n");
+}
+break;
 		}
 
 		}
 
 		
-
 		
-
 		
-
-		}
-
-		catch(Exception error) {System.out.println("Error en la lectura del archivo");}
-
 		
-
 		
-
 		
-
-		}
-
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		else if (respuestados.equals("4")) {
-
-		File usuarios = new File(RutaUsuarios);
 
 		try {
 
@@ -831,7 +525,7 @@ public class Tallers1 {
 
 		try {
 
-		Scanner scaregistros = new Scanner(new File(RutaRegistros));
+		Scanner scaregistros = new Scanner(registros);
 
 		String[] lineas = new String[300];
 
@@ -870,53 +564,20 @@ public class Tallers1 {
 		}
 
 
-
-
-
 		} //en este corchete termina el if(opcion1)
 
 		else if (respuesta1 == 2) {
 
-		
-
-
-
-
-
 		System.out.println("Bienvenido al menu de analisis");
 
 
-
-		
-
-
-
 		System.out.println("1) Actividad más realizada\r\n"
-
-
-
 		+ "2) Actividad más realizada por cada usuario\r\n"
-
-
-
 		+ "3) Usuario con mayor procastinacion\r\n"
-
-
-
 		+ "4) Ver todas las actividades\r\n"
-
-
-
 		+ "5) Salir");
 
-
-
 		int respuestaalgo;
-
-
-
-
-
 
 
 		while(true){
@@ -925,48 +586,21 @@ public class Tallers1 {
 
 		System.out.println("\nQue deseas realizar?: ");
 
-
-
 		try {
-
-
 
 		respuestaalgo = scanner.nextInt();
 
-
-
 		if(respuestaalgo>= 1 && respuestaalgo <= 5) {
-
-
 
 		break;
 
-
-
 		}
-
-
 
 		else {
 
-
-
 		System.out.println("Numero invalido");
 
-
-
 		}
-
-
-
-		
-
-
-
-		
-
-
-
 		} catch (InputMismatchException fg) {
 
 
@@ -977,61 +611,24 @@ public class Tallers1 {
 
 		scanner.nextLine();
 
-
-
-		
-
-
-
-		
-
-
-
 		}
 
 
-
-
-
-
-
 		}
-
-
-
-
-
 
 
 		if (respuestaalgo == 5) {
 
 		break;
-
-
-
-		
-
-
-
 		}
 
-
-
-		
-
-		File registros = new File(RutaRegistros);
 
 
 
 		if (respuestaalgo == 4) {
 
-
-
 		try {
 
-
-
-		
 
 		Scanner scaRegistro = new Scanner(registros);
 
@@ -1039,65 +636,30 @@ public class Tallers1 {
 
 		while(scaRegistro.hasNextLine()) {
 
-
-
 		String linea = scaRegistro.nextLine();
-
-
 
 		System.out.println(linea);
 
-
-
 		}
-
-
 
 		scaRegistro.close();
 
-
-
 		}
-
-
 
 		catch (FileNotFoundException adad) {
 
-
-
 		System.out.println("Archivo no encontrado");
 
-
-
 		}
-
-
-
-		
-
-
 
 		System.out.println("");
 
-
-
 		}
-
-
-
-		
-
 
 
 		if (respuestaalgo == 1) {
 
-
-
 		try {
-
-
-
-		
 
 		Scanner scaRegistro = new Scanner(registros);
 
@@ -1105,32 +667,15 @@ public class Tallers1 {
 
 		String[] actividade = new String[300];
 
-
-
 		int[] horaa = new int[300];
-
-
 
 		while(scaRegistro.hasNextLine()) {
 
-
-
 		String linea = scaRegistro.nextLine();
-
-
-
-		
-
-
 
 		String[] partes = linea.split(";");
 
-
-
 		String act = partes[3];
-
-
-
 		int horas = Integer.parseInt(partes[2]);
 
 		boolean existe = false;
@@ -1146,12 +691,6 @@ public class Tallers1 {
 		existe = true;
 
 		break;}
-
-		
-
-		
-
-
 
 		}
 
@@ -1278,6 +817,9 @@ public class Tallers1 {
 		}
 
 		}
+
+
+
 		}
 
 		}
@@ -1315,7 +857,11 @@ public class Tallers1 {
 		break;
 
 		}
+
 		}
+
+
+
 		}
 
 		}
@@ -1353,9 +899,15 @@ public class Tallers1 {
 		break;
 
 		}
+
 		}
+
+
+
 		}
+
 		}
+
 		}
 
 		scaregistro.close();
@@ -1405,15 +957,27 @@ public class Tallers1 {
 		maximo3 = actividadesEstefania[i];
 
 		}
+
+
+
 		}
+
 		System.out.println("-------------------------------------------");
+
 		System.out.println("La actividad mas realizada por Martin fue "+maximo1+"\nCon "+max1+" horas\n");
+
 		System.out.println("La actividad mas realizada por Catalina fue "+maximo2+"\nCon "+max2+" horas\n");
+
 		System.out.println("La actividad mas realizada por Estefania fue "+maximo3+"\nCon "+max3+" horas");
 		System.out.println("-------------------------------------------");
 
+	
 		}
+
 		catch(FileNotFoundException adad) {System.out.println("Archivo no encontrado");}
+
+		
+
 		}
 
 		if (respuestaalgo == 3){
@@ -1437,15 +1001,22 @@ public class Tallers1 {
 		if (partes[0].equals("Martin")) {
 
 		horasMartin += Integer.parseInt(partes[2]);
+
 		}
+
 		else if (partes[0].equals("Catalina")) {
+
 		horasCatalina += Integer.parseInt(partes[2]);
+
 		}
+
 		else if (partes[0].equals("Estefania")) {
+
 		horasEstefania += Integer.parseInt(partes[2]);
 
 		}
 		}
+
 		scaregistro.close();
 
 		System.out.println("---------------------------------------------------");
@@ -1469,28 +1040,48 @@ public class Tallers1 {
 		}
 
 		else if (horasCatalina == horasEstefania && horasCatalina > horasMartin) {
+
 		System.out.println("Empate entre Catalina y Estefania con "+horasCatalina);
+
 		}
+
 		else if (horasMartin > horasCatalina && horasMartin > horasEstefania) {
+
 		System.out.println("Martin es el usuario mas procastinador con "+horasMartin+" horas");
+
 		}
+
 		else if (horasCatalina > horasMartin && horasCatalina > horasEstefania) {
+
 		System.out.println("Catalina es el usuario mas procastinador con "+horasCatalina+" horas");
+
 		}
+
 		else {
+
 		System.out.println("Estefania es el usuario mas procastinador con "+horasEstefania+" horas");
+
 		}
+
 		System.out.println("---------------------------------------------------");
+
 		}
+
 		catch(FileNotFoundException aafqf) {System.out.println("Archivo no encontrado");}
 
 		}
 
 		}
+
+
 		}
 
 		scanner.close();
 
 		System.out.println("Gracias por usar nuestros servicios");
+
+
 		}
+
 	}
+
